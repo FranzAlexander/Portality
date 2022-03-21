224ec8c3b49790da933587f1fdcf8cb619dd0bf2
FranzAlexander <alexbalcombe4@gmail.com>
2022-03-21 12:56:01 +0800

    Portal Spawning at hand position with rotation

--- Portality/Assets/Scripts/Characters/Player.cs
+++ Portality/Assets/Scripts/Characters/Player.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _portal;
 
-    private const int MAX_PORTAL_AMOUNT = 2;
-    private int _currentPortalAmount;
+    // First index is left hand, second is for the right hand.
+    private GameObject[] _playerPortals;
+
 
     private void Awake()
     {
-        _currentPortalAmount = 0;
+        _playerPortals = new GameObject[2];
     }
 
     // Start is called before the first frame update
@@ -23,5 +26,28 @@ public class Player : MonoBehaviour
     void Update()
     {
 
+    }
+
+    public void createPortal(string handSide, Quaternion handRotation, Vector3 handPosition)
+    {
+        if (handSide == "Right")
+        {
+            if (_playerPortals[1] != null)
+            {
+                GameObject.Destroy(_playerPortals[1]);
+            }
+
+            _playerPortals[1] = Instantiate(_portal, handPosition, handRotation);
+
+        }
+        else
+        {
+            if (_playerPortals[0] != null)
+            {
+                GameObject.Destroy(_playerPortals[0]);
+            }
+            _playerPortals[0] = Instantiate(_portal, handPosition, handRotation);
+
+        }
     }
 }
